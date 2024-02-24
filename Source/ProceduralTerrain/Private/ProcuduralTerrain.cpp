2bdac48e07743333bfe3f09900fc51fd159ddec9
ArthurValladares <arthurkaukal@gmail.com>
2024-02-24 11:44:43 -0800

    much simpler mesh creation

--- Source/ProceduralTerrain/Private/ProcuduralTerrain.cpp
+++ Source/ProceduralTerrain/Private/ProcuduralTerrain.cpp
@@ -24,7 +24,7 @@ void AProcuduralTerrain::BeginPlay()
 
 	MaterialInstance = UMaterialInstanceDynamic::Create(Material, Mesh);
 	check(MaterialInstance);
-	MaterialInstance->SetTextureParameterValue("NoiseTexture", Noise.NoiseTexture);
+	//MaterialInstance->SetTextureParameterValue("NoiseTexture", Noise.NoiseTexture);
 	Mesh->SetMaterial(0, MaterialInstance);
 
 	CreateTriangle();
@@ -51,32 +51,11 @@ void AProcuduralTerrain::CreateTriangle() {
 	Triangles.Add(1);
 	Triangles.Add(3);
 
-	TArray<FVector> Normals;
-	Normals.Add(FVector(1, 0, 0));
-	Normals.Add(FVector(1, 0, 0));
-	Normals.Add(FVector(1, 0, 0));
-	Normals.Add(FVector(1, 0, 0));
-
 	TArray<FVector2D> Uv0;
-	Uv0.Add(FVector2D(0, 0));
-	Uv0.Add(FVector2D(1, 0));
-	Uv0.Add(FVector2D(0, 1));
-	Uv0.Add(FVector2D(1, 1));
+	Uv0.Add(FVector2D(0., 0.));
+	Uv0.Add(FVector2D(1., 0.));
+	Uv0.Add(FVector2D(0., 1.));
+	Uv0.Add(FVector2D(1., 1.));
 
-	TArray<FProcMeshTangent> Tangents;
-	Tangents.Add(FProcMeshTangent(0, 1, 0));
-	Tangents.Add(FProcMeshTangent(0, 1, 0));
-	Tangents.Add(FProcMeshTangent(0, 1, 0));
-	Tangents.Add(FProcMeshTangent(0, 1, 0));
-
-	TArray<FLinearColor> VertexColors;
-	VertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
-	VertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
-	VertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
-	VertexColors.Add(FLinearColor(0.75, 0.75, 0.75, 1.0));
-
-	Mesh->CreateMeshSection_LinearColor(0, Vertices, Triangles, Normals, Uv0, VertexColors, Tangents, true);
-
-	// Enable collision data
-	Mesh->ContainsPhysicsTriMeshData(true);
+	Mesh->CreateMeshSection_LinearColor(0, Vertices, Triangles, {}, Uv0, {}, {}, false);
 }
